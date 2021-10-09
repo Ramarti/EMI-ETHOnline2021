a760650b94a1b7fe2f931f2c5438c184c0ce931e
Raul <raul.mf86@gmail.com>
2021-10-09 13:46:05 +0200

    fund creation

--- scripts/create-fund.js
+++ scripts/create-fund.js
@@ -3,18 +3,24 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
-const enzyme = require("@enzymefinance/protocol");
-const { BigNumber } = require("@ethersproject/bignumber");
+const hre = require('hardhat')
+const enzyme = require('@enzymefinance/protocol')
+const ethers = hre.ethers
+const fs = require('fs')
+const { BigNumber } = ethers
 
-const ethers = hre.ethers;
 const ABIs = {
-  FUND_DEPLOYER: require("../external_abi/enzyme/FundDeployer.json"),
-};
+  FUND_DEPLOYER: require('../external_abi/enzyme/FundDeployer.json'),
+}
 const addresses = {
-  FUND_DEPLOYER: "0x7e6d3b1161DF9c9c7527F68d651B297d2Fdb820B",
-  DAI: "0x6b175474e89094c44da98b954eedeac495271d0f",
-};
+  FUND_DEPLOYER: '0x7e6d3b1161DF9c9c7527F68d651B297d2Fdb820B',
+  MANAGEMENT_FEE: '0x45f1b268cc6412b454dae20f8971fc4a36af0d29',
+  PERFORMANCE_FEE: '0x3c3f9143A15042B69eB314Cac211688A4E71a087',
+  ENTRANCE_BURN_FEE: '0x27F74B89B29508091014487253d8D9b88aa0264A',
+  ASSET_BLACKLIST: '0xdC1e40174ad831e505E8191881A66e90c3681E33',
+  DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  WETHER: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+}
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -26,47 +32,110 @@ async function main() {
 
   // We get the contract to deploy
 
-  const [ownerSigner] = await ethers.getSigners();
-  const timelock24h = 86400;
-  const feesPolicies =
-    "0x0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000010000000000000000000000007e8e91fcf0ad73e20adee711eb9e21fe65b90e600000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000033b2e3ca43cbacbde873158"; //await FundDeployer.createNewFund(owner, 'EMI', addresses.DAI, timelock24h, bytes _feeManagerConfigData, bytes _policyManagerConfigData)
+  const [ownerSigner] = await ethers.getSigners()
+  const timelock24h = '86400'
 
-  console.log("Instantiating fun deployer in ", addresses.FUND_DEPLOYER);
-  const fundDeployer = new ethers.Contract(
-    addresses.FUND_DEPLOYER,
-    ABIs.FUND_DEPLOYER,
-    ownerSigner
-  );
-  console.log("Connected");
-  console.log("fundDeployer owner", await fundDeployer.getOwner())
-  console.log("Creating fund...");
-  const comptrollerAddress = await fundDeployer.callStatic.createNewFund(
+  console.log('Instantiating fun deployer in ', addresses.FUND_DEPLOYER)
+  const fundDeployer = new ethers.Contract(addresses.FUND_DEPLOYER, ABIs.FUND_DEPLOYER, ownerSigner)
+  console.log('Connected')
+  console.log('Creating fee config...')
+  const feeManagerConfig = createFeeConfig()
+  console.log('Creating policy config...')
+
+  const policyManagerConfig = createPoliciesConfig()
+
+  console.log('Creating fund...')
+  const [comptrollerProxy, vaultProxy] = await fundDeployer.callStatic.createNewFund(
     ownerSigner.address,
-    "EMI",
-    addresses.DAI,
-    BigNumber.from(`${timelock24h}`),
-    feesPolicies,
-    "0x0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-  );
-  console.log("ComptrollerAddress", comptrollerAddress);
-  
-  const tx = await fundDeployer.callStatic.createNewFund(
+    'EMI',
+    addresses.WETHER,
+    BigNumber.from(timelock24h),
+    feeManagerConfig,
+    policyManagerConfig
+  )
+
+  const tx = await fundDeployer.createNewFund(
     ownerSigner.address,
-    "EMI",
-    addresses.DAI,
-    BigNumber.from(`${timelock24h}`),
-    feesPolicies,
-    "0x0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-    //"0x00000000000000000000000000000000000000000000000000000000000000"
-  );
-  console.log("tx", tx);
-  await tx.wait();
-  console.log("tx finished", tx);
+    'EMI',
+    addresses.WETHER,
+    BigNumber.from(timelock24h),
+    feeManagerConfig,
+    policyManagerConfig
+  )
+  await tx.wait()
+  console.log('Fund created!')
+  const toSave = { comptrollerProxy, vaultProxy }
+
+  console.log('Saving deployed addressess...', toSave)
+  console.log('Saved to file', saveDeployment(toSave))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
+
+function saveDeployment(results) {
+  const saveJson = JSON.stringify(results, null, 4)
+  const filePath = './deployment/fund.json'
+  fs.writeFileSync(filePath, saveJson, 'utf8')
+  return filePath
+}
+
+function getTokenBlacklist() {
+  return [addresses.DAI]
+}
+
+function createPoliciesConfig() {
+  // policies
+  // const maxConcentrationSettings = enzyme.maxConcentrationArgs(utils.parseEther('1'))
+  // const adapterBlacklistSettings = enzyme.adapterBlacklistArgs([])
+  // const adapterWhitelistSettings = enzyme.adapterWhitelistArgs([])
+  // TODO get from TCR contract
+  const assetBlacklistSettings = enzyme.assetBlacklistArgs(getTokenBlacklist())
+
+  const policyManagerConfig = enzyme.policyManagerConfigArgs({
+    policies: [
+      // fork.deployment.maxConcentration,
+      // fork.deployment.adapterBlacklist,
+      // fork.deployment.adapterWhitelist,
+      addresses.ASSET_BLACKLIST,
+    ],
+    settings: [
+      // maxConcentrationSettings,
+      // adapterBlacklistSettings,
+      // adapterWhitelistSettings,
+      assetBlacklistSettings,
+    ],
+  })
+  return policyManagerConfig
+}
+
+function createFeeConfig() {
+  const rate = ethers.utils.parseEther('0.01')
+  const scaledPerSecondRate = enzyme.convertRateToScaledPerSecondRate(rate)
+  const managementFeeSettings = enzyme.managementFeeConfigArgs(scaledPerSecondRate)
+  /*
+  const performanceFeeSettings = enzyme.performanceFeeConfigArgs({
+    rate: utils.parseEther('0.0'),
+    period: 365 * 24 * 60 * 60,
+  })
+  */
+  // const entranceRateFeeSettings = enzyme.entranceRateFeeConfigArgs(ethers.utils.parseEther('0.0'))
+
+  const feeManagerConfig = enzyme.feeManagerConfigArgs({
+    fees: [
+      addresses.MANAGEMENT_FEE,
+      //  addresses.PERFORMANCE_FEE,
+      //  addresses.ENTRANCE_BURN_FEE
+    ],
+    settings: [
+      managementFeeSettings,
+      // performanceFeeSettings,
+      // entranceRateFeeSettings
+    ],
+  })
+  return feeManagerConfig
+}
